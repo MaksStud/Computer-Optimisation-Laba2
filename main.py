6762089ff72fd1316f4c5e270de3f9eac1f1b293
MaksStud <studitskyi.maksym@internet-techonlogy-hub.com>
2025-03-19 21:09:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from simplex import SimplexMethod
+from scipy.optimize import linprog
+
+
+# Дані для задачі 1
+A = [
+  [1, 1, 0, -1],
+  [1, -1, 0, 3],
+  [2, 1, 1, -1]
+]
+b = [3, -1, 5]
+c = [-1, -1, -1, -5]
+
+
+# Дані для задачі 2
+"""A = [
+    [1, 8, 7, -15],
+    [1, -5, -6, 11]
+]
+b = [17, -9]
+c = [-4,- 3, -5, 20]"""
+
+simplex = SimplexMethod(A, b, c)
+optimal_value, coordinates = simplex.solve()
+print("Задача 1 (мої код):")
+print("Оптимальне рішення:", coordinates)
+print("Оптимальне значення функції:", optimal_value)
+
+
+res1 = linprog(c, A_eq=A, b_eq=b, method='highs')
+print("\nРезультати scipy.optimize.linprog:")
+print("Оптимальне рішення:", res1.x)
+print("Оптимальне значення функції:", res1.fun)
---
+++ simplex.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+class SimplexMethod:
+    def __init__(self, A, b, c):
+        self.A = np.array(A, dtype=float)
+        self.b = np.array(b, dtype=float)
+        self.c = np.array(c, dtype=float)
+        self.num_constraints, self.num_variables = self.A.shape
+        self.tableau = self._initialize_tableau()
+        self.basis = list(range(self.num_variables, self.num_variables + self.num_constraints))
+
+    def _initialize_tableau(self):
+        tableau = np.zeros((self.num_constraints + 1, self.num_variables + self.num_constraints + 1))
+        tableau[:-1, :-1] = np.hstack((self.A, np.eye(self.num_constraints)))
+        tableau[:-1, -1] = self.b
+        tableau[-1, :-1] = np.hstack((self.c, np.zeros(self.num_constraints)))
+        return tableau
+
+    def _get_pivot_column(self):
+        return np.argmin(self.tableau[-1, :-1])
+
+    def _get_pivot_row(self, pivot_col):
+        ratios = self.tableau[:-1, -1] / self.tableau[:-1, pivot_col]
+        ratios[ratios <= 0] = np.inf  # Уникнення від'ємних або нульових значень
+        return np.argmin(ratios)
+
+    #def _get_pivot_row(self, pivot_col):
+    #    column = self.tableau[:-1, pivot_col]
+    #    positive_mask = column > 0  # Враховуємо лише додатні значення
+    #    if not np.any(positive_mask):  # Якщо немає позитивних значень, рішення може бути необмеженим
+    #        raise ValueError("Розв’язок необмежений")
+    #    ratios = np.full_like(column, np.inf, dtype=float)  # Заповнюємо inf, щоб уникнути помилок
+    #    ratios[positive_mask] = self.tableau[:-1, -1][positive_mask] / column[positive_mask]
+    #    return np.argmin(ratios)
+
+    def _pivot(self, pivot_row, pivot_col):
+        self.tableau[pivot_row, :] /= self.tableau[pivot_row, pivot_col]
+        for i in range(len(self.tableau)):
+            if i != pivot_row:
+                self.tableau[i, :] -= self.tableau[i, pivot_col] * self.tableau[pivot_row, :]
+        self.basis[pivot_row] = pivot_col
+
+    def solve(self):
+        while np.any(self.tableau[-1, :-1] < 0):
+            pivot_col = self._get_pivot_column()
+            pivot_row = self._get_pivot_row(pivot_col)
+            if np.all(self.tableau[:-1, pivot_col] <= 0):
+                raise ValueError("Розв’язок необмежений")
+            self._pivot(pivot_row, pivot_col)
+        return -self.tableau[-1, -1], self._get_solution()
+
+    def _get_solution(self):
+        solution = np.zeros(self.num_variables)
+        for i, var_index in enumerate(self.basis):
+            if var_index < self.num_variables:
+                solution[var_index] = self.tableau[i, -1]
+        return solution
